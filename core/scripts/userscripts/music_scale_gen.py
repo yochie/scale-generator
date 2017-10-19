3903c4fa3b93f24b1c25118105c2c90bd68c82c1
Yoann Ochietti <yoann.ochietti@gmail.com>
2017-10-19 15:56:45 -0400

    Changed format for pagefromfile script and added upload to wiki

--- core/scripts/userscripts/music_scale_gen.py
+++ core/scripts/userscripts/music_scale_gen.py
@@ -1,4 +1,13 @@
 from music21 import *
+import pywikibot
+from scripts import pagefromfile
+import string
+
+pagefile = "toUpload.txt"
+sep = "@@@@@\n"
+
+fh = open(pagefile, "w")
+
 us = environment.UserSettings()
 us['lilypondPath'] = 'E:\\apps\\LilyPond\\usr\\bin\\lilypond.exe'
 
@@ -25,8 +34,26 @@ for currentScale in baseScales:
     
 lpc = lily.translate.LilypondConverter()
 for s in allScales:
-    htmltext = """<score vorbis="1">\\relative """ + lpc.lyPitchFromPitch(s.getTonic()).noteNamePitch + " { \\key " + lpc.lyPitchFromPitch(s.getTonic()).noteNamePitch + " \\" + s.type + " " + " ".join([lpc.lyPitchFromPitch(p).noteNamePitch + "4" for p in s.getPitches()]) + " }</score>"
+    title = """'''""" + s.getTonic().name.replace("#","+") + " " + s.type + """'''\n"""
+    lynote = lpc.lyPitchFromPitch(s.getTonic()).noteNamePitch
+    attribute0 = "<noinclude>[[Category:Gamme]]</noinclude>\n"
+    attribute1 = """{{#set: Est une gamme du mode=""" + s.type + "}}\n"
+    if s.getTonic().alter == 1.0 :
+        accidental = "+"
+    elif s.getTonic().alter == -1.0:
+        accidental = "-"
+    else:
+        accidental = "none"
+    attribute2 = """{{#set: A l accent=""" + accidental + "}}\n"
+    attribute3 = """{{#set: A la racine=""" + s.getTonic().name[0] + "}}\n"
+    attributes = attribute0 + attribute1 + attribute2 + attribute3
+    htmltext = sep + title + attributes + """<score vorbis="1">\\relative """ + lynote + " { \\key " + lynote + " \\" + s.type + " " + " ".join([lpc.lyPitchFromPitch(p).noteNamePitch + "4" for p in s.getPitches()]) + " }</score>\n" + sep
+    
+    fh.write(htmltext)
     print(htmltext)
 
+fh.close()
+
+pagefromfile.main('-file:' + pagefile ,'-begin:' + '@@@@@', '-end:' + '@@@@@', '-notitle', '-force', '-putthrottle:1')
 
 
